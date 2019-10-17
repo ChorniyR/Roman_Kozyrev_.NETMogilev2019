650671bfbf817411644c802518dc8e24ea6460c4
Roman Kozyrev <pomak803@gmail.com>
2019-10-17 21:02:07 +0300

    Task 2 module 2 was completed

---
+++ Module2_2/Module2_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            number = Int32.Parse(Console.ReadLine());
+
+            if((number%2==0) && (number >= 18))
+            {
+                Console.WriteLine("Сongratulate! You are adult");
+            }
+            else if((number%2!=0) && (number < 18) && (number > 13))
+            {
+                Console.WriteLine("Сongratulate with your going to hight school!");
+            }
+        }
+    }
+}
