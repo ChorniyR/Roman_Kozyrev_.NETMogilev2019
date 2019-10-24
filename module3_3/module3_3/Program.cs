c7f7a136f8965108277a73c973e0f2346ba7d517
Roman Kozyrev <pomak803@gmail.com>
2019-10-24 23:27:49 +0300

    Task 3 module 3 was copmleted

---
+++ module3_3/module3_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            int current, previous1 = 0, previous2 = 0;  
+
+            count = Int32.Parse(Console.ReadLine());
+
+            for(var i = 0; i < count; i++)
+            {
+                current = previous1 + previous2;
+                previous2 = previous1;
+                previous1 = current;
+
+                if(previous1 == 0)
+                {
+                    previous1 = 1;
+                }
+
+                Console.WriteLine("{0} ", current);
+            }
+        }
+    }
+}
