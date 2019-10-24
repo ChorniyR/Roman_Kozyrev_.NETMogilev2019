bf34642e49343ea203e367b3a48e554b39c6fc92
Roman Kozyrev <pomak803@gmail.com>
2019-10-24 23:31:05 +0300

    Task 5 module 3 was copmleted

---
+++ module3_5/module3_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Int32.Parse(Console.ReadLine());
+            string number = input.ToString();
+            int size = number.Length;
+
+            char[] symbhol = Console.ReadLine().ToCharArray();
+            char[] elements = new char[size];
+
+            int j = 0;
+            for (var i = 0; i < number.Length; i++)
+            {                
+                if (number[i] != symbhol[0])
+                {
+                    elements[j] = number[i];
+                    j++;
+                }            
+            }
+
+            int newNumber =int.Parse(new string(elements));
+            
+            Console.WriteLine(newNumber);
+        }
+    }
+}
