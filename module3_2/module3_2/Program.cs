0ef51a1507a4f8eef3290d072b2d5f72e62ba6a4
Roman Kozyrev <pomak803@gmail.com>
2019-10-24 23:25:12 +0300

    Task 2 module 3 was copmleted

---
+++ module3_2/module3_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+
+            number1 = Int32.Parse(Console.ReadLine());
+            
+            for(var i=0; i < number1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+        }
+    }
+}
